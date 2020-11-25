0be617e0b35aa4809fbd8990d624a2c4922ab4b9
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-11-26 00:08:09 +0700

    update / fixing get all data with pagination and add insert data user

--- config/routes.go
+++ config/routes.go
@@ -10,8 +10,11 @@ func Routes(pg *pg.DB, r *mux.Router){
 	db := controllers.Db{Pg:pg}
 	r.HandleFunc("/user/login", db.UserLogin)
 	r.HandleFunc("/user/get-list-user", db.GetAllDataUser)
-	r.HandleFunc("/user/get-user", db.GetDataUser)
+	r.HandleFunc("/user/get-user-data", controllers.GetUserData)
 	r.HandleFunc("/user/insert-user", db.InsertDataUser)
 	r.HandleFunc("/user/update-user", db.UpdateDataUser)
 	r.HandleFunc("/user/delete-user", db.UpdateDataUser)
+
+	//add middleware with jwt security
+	r.Use(controllers.JwtAuth)
 }
--- controllers/usercontrollers.go
+++ controllers/usercontrollers.go
@@ -4,12 +4,14 @@ import(
 	"github.com/go-pg/pg"
 	"net/http"
 	"log"
+	"strconv"
 	"github.com/irvandandung/gokriyatest/models"
 	"github.com/irvandandung/gokriyatest/repositorys"
 	"github.com/irvandandung/gokriyatest/helpers"
 	jwt "github.com/dgrijalva/jwt-go"
 	"time"
 	"encoding/json"
+	"github.com/go-pg/pg/orm"
 )
 
 type Db struct{
@@ -26,13 +28,13 @@ func (db *Db) UserLogin(w http.ResponseWriter, r *http.Request) {
 
 	pg := db.Pg
 	if r.Method != "POST" {
-        http.Error(w, "Unsupported http method", http.StatusBadRequest)
+        http.Error(w, "Unsupported http method!", http.StatusBadRequest)
         return
     }
 
     username, password, ok := r.BasicAuth()
     if !ok {
-        http.Error(w, "Invalid username or password", http.StatusBadRequest)
+        http.Error(w, "Invalid username or password!", http.StatusBadRequest)
         return
     }
 
@@ -74,15 +76,121 @@ func (db *Db) UserLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (db *Db) GetAllDataUser(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+
+	pg := db.Pg
+	var user []models.Users
+	var err error
+	if r.Method != "GET" {
+        http.Error(w, "Unsupported http method!", http.StatusBadRequest)
+        return
+    }
+
+    keys, ok := r.URL.Query()["page"]
+    if !ok {
+    	http.Error(w, "Please add parameter page!", http.StatusBadRequest)
+    	return 
+    }
+
+    page, _ := strconv.Atoi(keys[0])
+
+    limit := 5
+    offset := (page - 1) * 5
+
+    user, err = repositorys.GetAllDataUser(pg, user, limit, offset)
+    if err != nil{
+    	log.Println(err)
+    }
+
+    var data []interface{}
+    for _, val := range user {
+    	userdata := map[string]interface{}{
+	    	"Username" : val.Data.Username,
+	    	"Email" : val.Data.Email,
+	    	"Status" : val.Data.Status,
+    	}
+    	data = append(data, userdata)
+    }
+    log.Println(data)
+
+    json.NewEncoder(w).Encode(data)
 }
 
-func (db *Db) GetDataUser(w http.ResponseWriter, r *http.Request) {
-	
+func GetUserData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	userInfo := userInfo(r.Context().Value("userInfo").(jwt.MapClaims))
+	json.NewEncoder(w).Encode(userInfo)
 }
+
 
 func (db *Db) InsertDataUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	userInfo := userInfo(r.Context().Value("userInfo").(jwt.MapClaims))
+    pg := db.Pg
+	var user models.User
+	var users models.Users
+	var err error
+	var uuid string
+	var res orm.Result
+	var response map[string]interface{}
+
+	if r.Method != "POST" {
+        http.Error(w, "Unsupported http method!", http.StatusBadRequest)
+        return
+    }
+
+    role_name := userInfo["Role Name"].(string)
+    if role_name != "Admin"{
+    	http.Error(w, "Sorry, You can't access this request because you not admin!", http.StatusUnauthorized)
+        return
+    }
+
+    err = json.NewDecoder(r.Body).Decode(&user)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    _ , err = repositorys.GetDataUserByUsername(pg, users, user.Username)
+    if err == nil{
+    	http.Error(w, "Sorry, the username has been taken!", http.StatusUnauthorized)
+        return
+    }
+
+    uuid,err = helpers.UuidGenerate()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    user.Password, err = helpers.HashPassword(user.Password)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    userInsert := repositorys.Users{
+		Id : uuid,
+		Data : user,
+		Role_id : "d57bfbfe-4979-4809-a151-f6cd30de657b",
+	}
+	res, err = repositorys.InsertDataUser(pg, userInsert)
+
+	if res.RowsAffected() == 1 {
+		response = map[string]interface{}{
+			"status" : res.RowsAffected(),
+			"message" : "Insert success!",
+		}
+	}else{
+		response = map[string]interface{}{
+			"status" : res.RowsAffected(),
+			"message" : "Insert not success!",
+		}
+	}
 	
+	json.NewEncoder(w).Encode(response)
 }
 
 func (db *Db) UpdateDataUser(w http.ResponseWriter, r *http.Request) {
@@ -91,4 +199,19 @@ func (db *Db) UpdateDataUser(w http.ResponseWriter, r *http.Request) {
 
 func (db *Db) DeleteDataUser(w http.ResponseWriter, r *http.Request) {
 	
+}
+
+
+func userInfo(val jwt.MapClaims) map[string]interface{} {
+	user := val["User"].(map[string]interface {})
+	datauser := user["data"].(map[string]interface {})
+	role := user["Roles"].(map[string]interface {})
+	datarole := role["data"].(map[string]interface {})
+
+	return map[string]interface{}{
+		"User Id": user["id"],
+		"Username": datauser["username"],
+		"Email": datauser["email"],
+		"Role Name": datarole["role_name"],
+	}
 }
